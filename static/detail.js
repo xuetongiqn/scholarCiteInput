089ac2a93ca1990ec4af58260a34710858bf689b
xuetongiqn <xuetongqin@gmail.com>
2014-11-17 10:07:08 +0800

    不输入也可跳转

--- static/detail.js
+++ static/detail.js
@@ -1,14 +1,13 @@
 $(window).ready(function(){
     $('[name=citeNum]').select().focus().on('keyup',function(e){
         var userInput = $(this).val();
-        if(e.keyCode==13 && !isNaN(userInput)){
-            var id = $(this).attr('data-id');
+        var id = $(this).attr('data-id');
 
+        if(e.keyCode==13 && (!isNaN(userInput)){
             $.post('/submit/set_cite',{
                 id : id,
                 num : userInput
             },function(data){
-
                 if(data.change==1){
                     location.href = '/goto/next?id='+id
                 }else{
@@ -16,9 +15,8 @@ $(window).ready(function(){
                 }
             },'json')
 
-
-
-
+        }else if(userInput == ""){
+            location.href = '/goto/next?id='+id
         }
     });
 })
